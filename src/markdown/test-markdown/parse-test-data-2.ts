9fa67711e366a049f8440605a2d41e3973e84dac
tacogips <me@tacogips.me>
2024-09-30 11:53:40 +0900

    SomE

---
+++ src/markdown/test-markdown/parse-test-data-2.ts
@@ -0,0 +1,55 @@
+import { type Elem } from "~/markdown";
+
+const input = `
+
+---
+id: some
+aliases: []
+tags:
+  - clippings
+author:
+  - "[[@auth0]]"
+created: "2024-09-20"
+description: xxx
+source:  somewhere
+title: no
+---
+
+
+user
+---
+user text
+
+
+assistant
+---
+text
+
+`;
+
+const expected = [
+  {
+    text: "id: some\naliases: []\ntags:",
+    type: "p_without_link",
+  },
+  {
+    id: "user",
+    text: "user",
+    type: "h2",
+  },
+  {
+    text: "user text",
+    type: "p_without_link",
+  },
+  {
+    id: "assistant",
+    text: "assistant",
+    type: "h2",
+  },
+  {
+    text: "text",
+    type: "p_without_link",
+  },
+];
+
+export default [input, expected] as Array<string | Elem[]>;
--- src/markdown/test-markdown/parse-test-datas.ts
+++ src/markdown/test-markdown/parse-test-datas.ts
@@ -1,7 +1,8 @@
 import data1 from "./parse-test-data-1";
+import data2 from "./parse-test-data-2";
 import { type Elem } from "~/markdown";
 
-const datas: (string | Elem[])[][] = [data1];
+const datas: (string | Elem[])[][] = [data1, data2];
 
 //
 //     ★ parse_test1.ts
