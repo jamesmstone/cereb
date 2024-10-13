f9bd07b03a58154a242d6e367479e08d7c580bf6
tacogips <me@tacogips.me>
2024-10-13 21:44:06 +0900

    change model

---
+++ src/ai-service/openai/config.ts
@@ -0,0 +1,27 @@
+import { getConfigValue, type ConfigKey } from "~/config";
+
+export interface OpenAIConfig {
+  apiKey: string | undefined;
+  defaultMaxToken: number;
+}
+
+const apiSecretConfigKey: ConfigKey = {
+  envKey: "OPENAI_API_KEY",
+  configDataKey: "", //TODO(tacogips) define
+};
+
+const defaultMaxTokenNumber: ConfigKey = {
+  envKey: "OPENAI_MAX_TOKEN_NUM",
+  configDataKey: "", //TODO(tacogips) define
+};
+
+export function loadOpenAIConfig(): OpenAIConfig {
+  const apiKey = getConfigValue(apiSecretConfigKey);
+  const defaultMaxToken =
+    Number(getConfigValue(defaultMaxTokenNumber) || "") || 1024;
+
+  return {
+    apiKey,
+    defaultMaxToken,
+  };
+}
---
+++ src/lib.ts
@@ -0,0 +1,17 @@
+//import {
+//  type MessageBody,
+//  type MessageHistory,
+//  type QueryResponse,
+//  Role,
+//  newAiClientFromModel,
+//  newTextBody,
+//  type TokenUsage,
+//  emptyResponse,
+//} from "./ai-service";
+//import { pathOrUrlToAttachmentMessage } from "./attachment";
+//import { messagesFromMarkdown, messageBodyToMarkdown } from "./markdown";
+//
+//import { type QueryMessages } from "./query";
+//
+//import { Command } from "commander";
+//import getStdin from "get-stdin";
